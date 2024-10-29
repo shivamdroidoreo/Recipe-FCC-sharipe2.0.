e2d9683b45ff855d888c6c66435db98d4e50f7f1
shivambhartiya <shivam.bhartiya.btech2023@gmail.com>
2024-10-29 12:42:56 +0530

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,29 +1,24 @@
+import logo from './logo.svg';
 import './App.css';
-import Home from './components/Home';
-import Navbar from './components/Navbar.js';
-import About from './components/Aboutus.js';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
-
-
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <><Navbar/><Home /></>,
-  },
-  {
-    path:'/about',
-    element: <><Navbar/><About /></>,
-  },
-  // Add other routes here if needed
-]);
 
 function App() {
   return (
-    <>
-    
-    < RouterProvider router={router}/>
-    </>
-    
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
